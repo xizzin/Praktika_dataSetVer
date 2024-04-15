9b9f0e56a23e4ba95ce675bbc83d507623ce5f17
xizzin <xizzincol@gmail.com>
2024-04-15 14:26:04 +0300

    pr4 done

---
+++ AuthorsPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//
+using prOneDataSetVer.booksDataSetTableAdapters;
+//
+namespace prOneDataSetVer
+{
+
+    public partial class AuthorsPage : Page
+    {
+        AuthorsTableAdapter Authors = new AuthorsTableAdapter();
+        public AuthorsPage()
+        {
+            InitializeComponent();
+            AuthorsGrid.ItemsSource = Authors.GetData();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            AuthorsGrid.ItemsSource = Authors.SearchAuthors(SearchBar.Text);
+        }
+    }
+}
